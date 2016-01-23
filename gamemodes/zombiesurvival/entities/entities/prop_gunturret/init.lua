f62edf79b185a666f97a633904480f7a9432dd8a
C0nw0nk <C0nw0nks@googlemail.com>
2016-01-23 19:58:12 +0000

    Update init.lua

--- gamemodes/zombiesurvival/entities/entities/prop_gunturret/init.lua
+++ gamemodes/zombiesurvival/entities/entities/prop_gunturret/init.lua
@@ -199,8 +199,11 @@ function ENT:AltUse(activator, tr)
 end
 
 function ENT:OnPackedUp(pl)
-	pl:GiveEmptyWeapon("weapon_zs_gunturret")
-	pl:GiveAmmo(1, "thumper")
+	if pl:HasWeapon("weapon_zs_gunturret") then
+		pl:GiveAmmo(1, "thumper")
+	else
+		pl:Give("weapon_zs_gunturret")
+	end
 
 	pl:PushPackedItem(self:GetClass(), self:GetObjectHealth())
 	pl:GiveAmmo(self:GetAmmo(), "smg1")
